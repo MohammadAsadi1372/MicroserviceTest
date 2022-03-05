f4fbf51761d02a739f8bed4e31e65607e7f4b2cf
mohammad <mohammadasadima8@gmail.com>
2022-03-05 09:16:35 +0330

    AddGrpcClient And
    GrpcClientWebApp  is Client
    GrpcService  is Server

---
+++ GrpcClient2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Grpc.Net.Client;
+using GrpcService2;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:5071");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+                  new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine("Greeting: " + reply.Message);
---
+++ GrpcClientWebApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,49 @@
+﻿using Grpc.Net.Client;
+using GrpcClient;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcClientWebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+
+
+            using var channel2 = GrpcChannel.ForAddress("https://localhost:5071");
+            var ChangeOrderState = new ChangeState.ChangeStateClient(channel2);
+            var reply2 =  ChangeOrderState.ChangeOrderState(new ChangeStateRequest { OrderId = "asdasdasd", OrderState = 1, OrderStatus = 2 });
+
+
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
