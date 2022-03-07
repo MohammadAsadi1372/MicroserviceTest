268effc002508b16a96e690aca369997b977fd54
mohammad <mohammadasadima8@gmail.com>
2022-03-07 09:18:57 +0330

    Set Commited

--- GrpcClient2/Program.cs
+++ GrpcClient2/Program.cs
@@ -6,6 +6,7 @@ using GrpcService2;
 
 using var channel = GrpcChannel.ForAddress("https://localhost:5071");
 var client = new Greeter.GreeterClient(channel);
+
 var reply = await client.SayHelloAsync(
                   new HelloRequest { Name = "GreeterClient" });
 Console.WriteLine("Greeting: " + reply.Message);
